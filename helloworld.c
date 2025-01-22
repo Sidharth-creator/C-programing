1d14de7d3c9243d878bddaf18dd2ac6a0280ec53
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2025-01-22 16:29:31 +0530

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  float num1,num2,sum;
+  printf("\nenter number 1:");
+  scanf("%f",&num1);
+  printf("\nenter number 2:");
+  scanf("%f",&num2);
+  sum=num1+num2;
+  printf("%f+%f=%f\n",num1,num2,sum);
+  return 0;
+}
